f8d2feee1bd6a4e1bf1be7c9d38b42d740ac3f72
Motlaq-Alnassafi <alnassafimotlaq@gmail.com>
2025-11-30 09:20:39 +0300

    swift formating

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -19,17 +19,17 @@ public protocol HTTPClient {
 public final class RemoteFeedLoader {
     private let url: URL
     private let client: HTTPClient
-    
+
     public enum Error: Swift.Error {
         case connectivity
         case invalidData
     }
-    
+
     public init(url: URL = URL(string: "https://a_url.com")!, client: HTTPClient) {
         self.client = client
         self.url = url
     }
-    
+
     public func load(completion: @escaping (Error) -> Void) {
         client.get(from: url) { result in
             switch result {
@@ -37,7 +37,7 @@ public final class RemoteFeedLoader {
                 completion(.invalidData)
             case .failure:
                 completion(.connectivity)
-               }
+            }
         }
     }
 }
--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -5,81 +5,81 @@
 //  Created by Motlaq Alnassafi on 26/11/2025.
 //
 
-import XCTest
 import EssentialFeed
+import XCTest
 
 class RemoteFeedLoaderTests: XCTestCase {
-    
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
-        
+
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
-    
+
     func test_load_requestsDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
-        
+
         sut.load { _ in }
-        
+
         XCTAssertEqual(client.requestedURLs, [url])
     }
-    
+
     func test_load_requestsDataFromURLTwice() {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
-        
+
         sut.load { _ in }
         sut.load { _ in }
-        
+
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
-    
+
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
         var capturedErrors = [RemoteFeedLoader.Error]()
         sut.load { capturedErrors.append($0) }
-        
+
         let clientError = NSError(domain: "test", code: 0)
         client.complete(with: clientError)
-        
+
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
-    
+
     func test_load_deliversErrorOnNon200HTTPResponse() {
         let (sut, client) = makeSUT()
         let samples = [199, 201, 300, 400, 500]
-        
-        samples.enumerated().forEach { index, statusCode in
+
+        for (index, statusCode) in samples.enumerated() {
             var capturedErrors = [RemoteFeedLoader.Error]()
             sut.load { capturedErrors.append($0) }
             client.complete(withStatusCode: statusCode, at: index)
             XCTAssertEqual(capturedErrors, [.invalidData])
         }
     }
-    
+
     // MARK: - Helpers
+
     private func makeSUT(url: URL = URL(string: "https://a_url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: client)
         return (sut, client)
     }
-    
+
     private class HTTPClientSpy: HTTPClient {
         private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
-        
+
         var requestedURLs: [URL] {
-            return messages.map {$0.url}
+            return messages.map { $0.url }
         }
-        
+
         func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
             messages.append((url, completion))
         }
-        
+
         func complete(with error: Error, at index: Int = 0) {
             messages[index].completion(.failure(error))
         }
-        
+
         func complete(withStatusCode statusCode: Int, at index: Int = 0) {
             let response = HTTPURLResponse(url: requestedURLs[index],
                                            statusCode: statusCode,
